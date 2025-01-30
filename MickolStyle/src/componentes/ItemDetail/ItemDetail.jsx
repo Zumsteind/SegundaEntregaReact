47a9eb8cb8105fd91b9b69d6c3a94f30c5fed26d
Zumsteind Facundo <zumsteind@gmail.com>
2025-01-30 15:51:41 -0300

    se Agrego Contador
    Se agregar contador. al ItemDetail

--- MickolStyle/src/componentes/ItemDetail/ItemDetail.jsx
+++ MickolStyle/src/componentes/ItemDetail/ItemDetail.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ItemDetail.css'
+import { Link } from 'react-router-dom';
+import Contador from '../Contador/Contador';
 
-const ItemDetail = ({id,nombre,precio,img}) => {
+const ItemDetail = ({id,nombre,stock,precio,img}) => {
+
+const [agregarCantidad,setAgregarCantidad]=useState(0);
+
+  const manejadorDeCantidad = (cantidad)=>{
+    setAgregarCantidad(cantidad);
+    console.log("La cantidad de productos agregador es: "+ cantidad);
+
+  }
+
   return (
     <div className='ContenedorItem'>
         
@@ -11,6 +22,8 @@ const ItemDetail = ({id,nombre,precio,img}) => {
         <p>Codigo: {id}</p>
         <img src={img} alt={nombre}  />
         <p>Remera de Calidad Premiun.</p>
+
+        {agregarCantidad > 0 ? (<Link to="/cart">Terminar Compra</Link>): (<Contador inicial={1} stock={stock} funcionAgregar={manejadorDeCantidad} />) }
 
     </div>
   )
