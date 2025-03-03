f95a39e1833a201c872940d11e39b3915a89d384
Zumsteind Facundo <zumsteind@gmail.com>
2025-03-03 16:20:57 -0300

    imag carrito
    se modifica imagen carrito

--- MickolStyle/src/componentes/CartWidget/CartWidget.jsx
+++ MickolStyle/src/componentes/CartWidget/CartWidget.jsx
@@ -6,7 +6,7 @@ import { CarritoContext } from '../Context/CarritoContext'
 
 const CartWidget = () => {
             
-  const carritoImage = "https://img.freepik.com/vector-premium/plantilla-vector-icono-carrito-compras-color-dorado_917138-3031.jpg"
+  const carritoImage = "https://imgs.search.brave.com/Wqj7drI9_83FdNPKpuB3l9B7dfwL7CyeMAGAdelKoAY/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMudmVjdGVlenku/Y29tL3N5c3RlbS9y/ZXNvdXJjZXMvdGh1/bWJuYWlscy8wNTUv/NjU5Lzc2Ni9zbWFs/bC9nb2xkZW4tc2hv/cHBpbmctY2FydC1h/d2FpdGluZy1tZXJj/aGFuZGlzZS1wdXJj/aGFzZS1wbmcucG5n"
   const {cantidadTotal} = useContext(CarritoContext)
     return (
     
